ca704b65d0a3d3322d7f6e01511116528454ee5c
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-02-15 14:32:42 +0100

    added example config

--- nilms/views/login.py
+++ nilms/views/login.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from nilms.facades.user_facade import UserFacade
+from nilms.password import check_password
 
 
 bp = Blueprint(
@@ -12,6 +13,7 @@ bp = Blueprint(
 
 @bp.route('/login', methods=['POST', 'GET'])
 def login():
+    err_wrong_cred = 'Wrong credentials'
     errors = []
 
     if request.method == 'POST':
@@ -21,6 +23,10 @@ def login():
         existing = UserFacade.get(name=username)
 
         if not existing:
-            errors.append('Wrong credentials')
+            errors.append(err_wrong_cred)
+
+        if not len(errors):
+            if not check_password(existing['password'], password):
+                errors.append(err_wrong_cred)
 
     return render_template('admin/login.html', errors=errors)
