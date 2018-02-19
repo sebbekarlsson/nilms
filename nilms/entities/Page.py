cd3583445000c7b5e126955398d60bb4d54fbd0a
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-02-19 09:28:15 +0100

    added created_at datetime field for pages

--- nilms/entities/Page.py
+++ nilms/entities/Page.py
@@ -1,8 +1,16 @@
-from mongoengine import Document, StringField, DictField, BooleanField
+from mongoengine import (
+    Document,
+    StringField,
+    DictField,
+    BooleanField,
+    DateTimeField
+)
+import datetime
 
 
 class Page(Document):
     name = StringField(max_length=200)
     template = StringField(max_length=200)
     is_startpage = BooleanField(default=False)
+    created_at = DateTimeField(default=datetime.datetime.now())
     data = DictField()
