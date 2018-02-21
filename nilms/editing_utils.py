9260ef56f803eb248870913743ac34bf1f80cddf
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-02-21 13:16:24 +0100

    be able to visit single posts

--- nilms/editing_utils.py
+++ nilms/editing_utils.py
@@ -2,8 +2,9 @@ from flask import render_template
 from nilms.session_utils import is_loggedin
 
 
-def admin_navigation(page_id):
+def admin_navigation(post, page):
     if not is_loggedin():
-        return render_template('editing/visitor.html', page_id=page_id)
+        return render_template('editing/visitor.html', post=post, page=page)
 
-    return render_template('editing/admin_navigation.html', page_id=page_id)
+    return render_template('editing/admin_navigation.html',
+                           post=post, page=page)
--- nilms/views/index.py
+++ nilms/views/index.py
@@ -7,9 +7,22 @@ from nilms.theme_utils import get_theme_db
 from nilms.editing_utils import admin_navigation
 from jinja2 import Environment, FileSystemLoader
 import os
+from bson.objectid import ObjectId
 
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
+
+
+env = Environment(
+    loader=FileSystemLoader(os.path.join(config['theme_dir'], 'templates'))
+)
+
+env.globals.update(
+    admin_navigation=admin_navigation,
+    PageFacade=PageFacade,
+    PostFacade=PostFacade,
+    AssetFacade=AssetFacade
+)
 
 
 @bp.route('/<page_name>')
@@ -21,17 +34,19 @@ def show(page_name):
     if not page:
         return 'page not found', 404
 
-    env = Environment(
-        loader=FileSystemLoader(os.path.join(config['theme_dir'], 'templates'))
-    )
-
-    env.globals.update(
-        admin_navigation=admin_navigation,
-        PageFacade=PageFacade,
-        PostFacade=PostFacade,
-        AssetFacade=AssetFacade
-    )
-
     j_template = env.get_template(page.template)
 
     return j_template.render(db=get_theme_db(), page=page)
+
+
+@bp.route('/post/<post_id>')
+@bp.route('/', defaults={'post_id': None})
+def show_post(post_id):
+    post = PostFacade.get(id=ObjectId(post_id)) if post_id else None
+
+    if not post:
+        return 'post not found', 404
+
+    j_template = env.get_template(post.template)
+
+    return j_template.render(db=get_theme_db(), post=post, page=post)
