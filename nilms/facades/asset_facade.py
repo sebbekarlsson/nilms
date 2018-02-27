2b8d7bef8b9f07e2a07b3ed0463b9a258f67a0bd
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-02-27 14:49:31 +0100

    moved some code around

--- nilms/facades/asset_facade.py
+++ nilms/facades/asset_facade.py
@@ -1,4 +1,5 @@
 from nilms.entities.Asset import Asset
+from nilms.facades.post_facade import PostFacade
 from mongoengine.queryset import DoesNotExist
 
 
@@ -17,13 +18,27 @@ class AssetFacade(object):
     @staticmethod
     def delete(**kwargs):
         try:
-            return Asset.objects(**kwargs).delete()
+            assets = Asset.objects(**kwargs)
+
+            for _asset in assets:
+                posts = PostFacade.get_all(
+                    query={'assets': _asset}
+                )
+
+                for post in posts:
+                    if _asset in post.assets:
+                        post.assets.remove(_asset)
+
+                    post.update(assets=post.assets)
+
+            return assets.delete()
         except DoesNotExist:
             return False
 
     @staticmethod
     def get_all(order_by='-created_at', offset=None, limit=None, query={}):
-        _all = Asset.objects(**query).skip(offset).limit(limit).order_by(order_by)
+        _all = Asset.objects(**query).skip(offset)\
+            .limit(limit).order_by(order_by)
 
         return _all
 
--- nilms/views/api.py
+++ nilms/views/api.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from nilms.facades.page_facade import PageFacade
-from nilms.facades.post_facade import PostFacade
 from nilms.facades.asset_facade import AssetFacade
 from nilms.session_utils import login_required
 from bson.objectid import ObjectId
@@ -40,15 +39,4 @@ def show_page(page_id):
 
 @bp.route('/asset/delete/<asset_id>')
 def show_asset_delete(asset_id):
-    posts = PostFacade.get_all(
-        query={'assets': AssetFacade.get(id=ObjectId(asset_id))}
-    )
-
-    for post in posts:
-        for asset in post.assets:
-            if asset.id == ObjectId(asset_id):
-                post.assets.remove(asset)
-
-        post.update(assets=post.assets)
-
     return jsonify(AssetFacade.delete(id=ObjectId(asset_id)))
