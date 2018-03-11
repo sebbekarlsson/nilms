c8d88405a31e9950da70790d8ca68b9616af4dfe
sebastian karlsson <sebbekarlsson97@gmail.com>
2018-03-11 22:40:20 +0100

    show post kind in list

--- nilms/views/admin.py
+++ nilms/views/admin.py
@@ -144,7 +144,7 @@ def show_post(post_id):
     templates = get_theme_templates()
     assets = AssetFacade.get_all()
     theme_db = get_theme_db()
-    types = theme_db['post_kinds'] if 'post_kinds' in theme_db else []
+    kinds = theme_db['post_kinds'] if 'post_kinds' in theme_db else []
 
     if request.method == 'POST':
         if request.form.get('delete'):
@@ -155,12 +155,12 @@ def show_post(post_id):
             name = request.form.get('post-name')
             content = request.form.get('post-content')
             template = request.form.get('post-template')
-            post_type = request.form.get('post-kind')
+            post_kind = request.form.get('post-kind')
             is_published = request.form.get('post-is_published') is not None
             new_asset_files = request.files.getlist('new-assets')
             new_assets = []
 
-            post_type = None if not post_type else post_type
+            post_kind = None if not post_kind else post_kind
 
             for new_asset_file in new_asset_files:
                 filename = upload_file(new_asset_file)
@@ -180,7 +180,7 @@ def show_post(post_id):
                     name=name,
                     content=content,
                     template=template,
-                    kind=post_type,
+                    kind=post_kind,
                     is_published=is_published,
                     assets=new_assets
                 )
@@ -190,7 +190,7 @@ def show_post(post_id):
                     name=name,
                     content=content,
                     template=template,
-                    kind=post_type,
+                    kind=post_kind,
                     is_published=is_published,
                     assets=new_assets
                 )
@@ -200,7 +200,7 @@ def show_post(post_id):
         'admin/post.html',
         templates=templates,
         assets=assets,
-        types=types,
+        kinds=kinds,
         post=post
     )
 
