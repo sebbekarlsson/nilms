4b0d812806ea93b166e39a06da0c83e7cfcb8f11
sebastian karlsson <sebbekarlsson97@gmail.com>
2018-03-12 19:39:39 +0100

    correct mimetype when accessing static theme files, closes #13

--- nilms/views/theme.py
+++ nilms/views/theme.py
@@ -13,7 +13,27 @@ bp = Blueprint(
 
 @bp.route('/static/<path:file_path>')
 def show_file(file_path):
+    mimes = {
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.gif': 'image/gif',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.bmp': 'image/bmp',
+        '.xml': 'application/xml',
+        '.pdf': 'application/pdf'
+    }
+
+    filename, ext = os.path.splitext(file_path)
+    ext = ext.lower()
+
+    mime = mimes[ext] if ext in mimes else 'text/plain'
+
+    print(mime, ext)
+
     return send_from_directory(
         os.path.join(config['theme_dir'], 'static'),
-        file_path
+        file_path,
+        mimetype=mime
     )
